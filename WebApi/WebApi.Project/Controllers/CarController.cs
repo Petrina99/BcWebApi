16060c1170cde80c34e16aa110fc831336d59d30
Mislav-laptop <petrinamislav@gmail.com>
2024-05-29 12:38:13 +0200

    fixed list initialization

--- WebApi/WebApi.Project/Controllers/CarController.cs
+++ WebApi/WebApi.Project/Controllers/CarController.cs
@@ -8,20 +8,12 @@ namespace WebApi.Project.Controllers
     [ApiController]
     public class CarController : ControllerBase
     {
-        public List<Car> cars = new List<Car>();
+        static Car car1 = new Car(1, "BMW", "M5", 500, 2022, 120000);
+        static Car car2 = new Car(2, "Mercedes", "C class", 130, 2016, 200000);
+        static Car car3 = new Car(3, "Suzuki", "Swift", 90, 2023, 12000);
+        static Car car4 = new Car(4, "Audi", "A6", 270, 2019, 189400);
 
-        Car car1 = new Car(1, "BMW", "M5", 500, 2022, 120000);
-        Car car2 = new Car(2, "Mercedes", "C class", 130, 2016, 200000);
-        Car car3 = new Car(3, "Suzuki", "Swift", 90, 2023, 12000);
-        Car car4 = new Car(4, "Audi", "A6", 270, 2019, 189400);
-
-        public CarController()
-        {
-            cars.Add(car1);
-            cars.Add(car2);
-            cars.Add(car3);
-            cars.Add(car4);
-        }
+        public static List<Car> cars = new List<Car> { car1, car2, car3, car4 };
 
         [HttpGet(Name = "GetCars")]
         public IActionResult GetCars()
@@ -36,7 +28,7 @@ namespace WebApi.Project.Controllers
         }
         
         [HttpPost(Name="CreateCar")]
-        public IActionResult CreateCar(Car newCar)
+        public IActionResult CreateCar([FromBody] Car newCar)
         {
             if (newCar == null)
             {
@@ -69,7 +61,7 @@ namespace WebApi.Project.Controllers
         }
 
         [HttpDelete(Name = "DeleteCar")]
-        public IActionResult DeleteCar(int id) 
+        public IActionResult DeleteCar([FromQuery] int id) 
         { 
             Car carToRemove = cars.Find(x  => x.Id == id);
             if (carToRemove == null) { return BadRequest(); }
